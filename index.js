5aa1a853436626bf3ca0c09db25aff3d197c1386
Matt Dittes <mattdittes@gmail.com>
2022-07-31 12:01:51 -0400

    commit tree 576d01df279eff24f8010b0c7ebbc39cbbd0650d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === names.toLowerCase()
+    );
+};
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+        (possibleMatch) => 
+            possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+};
+
+function matchName(drivers, driverName) {
+    return drivers.filter((record) => record.name === driverName);
+}
